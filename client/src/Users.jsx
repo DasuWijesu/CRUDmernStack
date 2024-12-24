41151899d84b4753bcf57f630b1d65d2f07b89d9
DasuWijesu <127611557+DasuWijesu@users.noreply.github.com>
2024-12-24 23:05:13 +0530

    link the CreateUser page to User page with import{Link} from "React-router-dom"

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Users (){
     //to display data create variable 
@@ -8,6 +9,7 @@ function Users (){
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className="w-50 bg-white rounded p-3">
+                <Link to="/create" className='btn btn-success'>Add+</Link>
                 <table className="table">
                     <thead>
                         <tr>
